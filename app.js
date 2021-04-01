ae2567729323570ac65296dbe925118107ce2669
Ata-92 <atakal92@gmail.com>
2021-04-01 23:18:08 +0300

    Add immediately invoked function

--- app.js
+++ app.js
@@ -23,12 +23,23 @@
 
 // console.log(myFunction([1, 2, [4, 0]]));
 
-let cloneArray = [];
-const myFunction = function (arr) {
-  for (let num of arr) {
-    cloneArray.push(num);
-  }
-}
 
-console.log(myFunction([1, 2, [4, 0]]));
-console.log(cloneArray);
+// let cloneArray = [];
+// const myFunction = function (arr) {
+//   for (let num of arr) {
+//     cloneArray.push(num);
+//   }
+// }
+
+// console.log(myFunction([1, 2, [4, 0]]));
+// console.log(cloneArray);
+
+
+(function (arr) {
+  let cloneArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    cloneArray[i] = arr[i];
+  }
+  console.log(cloneArray);
+})([1, 2, [4, 0]])
+
