5432c850f1253c33450ee06b13b08887435553e2
Ata-92 <atakal92@gmail.com>
2021-04-01 22:59:38 +0300

    Add a for of solution

--- app.js
+++ app.js
@@ -15,10 +15,20 @@
 // console.log(cloner([1, 2, [4, 0]]));
 
 
-let cloneArray;
+// let cloneArray;
+// const myFunction = function (arr) {
+//   cloneArray = arr;
+//   console.log(cloneArray);
+// }
+
+// console.log(myFunction([1, 2, [4, 0]]));
+
+let cloneArray = [];
 const myFunction = function (arr) {
-  cloneArray = arr;
-  console.log(cloneArray);
+  for (let num of arr) {
+    cloneArray.push(num);
+  }
 }
 
 console.log(myFunction([1, 2, [4, 0]]));
+console.log(cloneArray);
